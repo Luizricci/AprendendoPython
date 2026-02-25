4187269a57f5255d7041e5285a96c50873554a5e
Luiz_Ricci <luizaureliano.tds1.senai@gmail.com>
2026-02-25 15:26:56 -0300

    Create Sistema de média

---
+++ main.py
@@ -0,0 +1,28 @@
+notas = []
+qntNotas = int(input("digite a qunatidade de notas: "))
+
+for x in range(qntNotas):
+    nota = float(input("digite a nota: "))
+    while True:
+        if nota < 0 or nota >10 :
+            print("a Nota do aluno não pode ser menor que 0 ou maior que 10 digite novamente")
+            nota = float(input("digite a nota: "))
+        else:
+            notas.append(nota)
+            break
+print("______________")
+
+
+for y in notas:
+    soma = sum(notas)
+    media= soma / qntNotas
+
+if media >= 7 :
+    print("APROVADO SEU SAFADO")
+else:
+    print("REPROVOU BURRÃO ;)")
+
+
+
+
+
